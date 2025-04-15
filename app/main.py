071d77c0658a9b7298802499e856c084c2ee68e6
Арыбжан Азизов <arybzhanazizov@MacBook-Air-Arybzan.local>
2025-04-16 00:30:33 +0600

    fix bags and change routers

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from api.routers.todo import todo_router
+from tortoise.contrib.fastapi import register_tortoise
 
 
 app = FastAPI()
 app.include_router(todo_router)
+register_tortoise(
+    app=app,
+    db_url="sqlite://todo.db",
+    add_exception_handlers=True,
+    generate_schemas=True,
+    modules={"models":["api.models.todo_models"]}
+)
 
 
 @app.get("/")
